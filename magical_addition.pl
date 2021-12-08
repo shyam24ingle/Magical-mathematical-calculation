5884d3df46899c856046aa1d143997ed438331b8
shyam24ingle <bioinfo.sham@gmail.com>
2021-12-08 16:19:51 +0530

    Add files via upload

---
+++ magical_addition.pl
@@ -0,0 +1,20 @@
+#Addition program
+
+print"\n Hi friends!! ***This is Addition program***\n";
+print"\n This is very interesting mathematical program.\n In this program computer will give Answer before calculation.";
+print"\n This is a Four step  program and final Answer of this program is 1998";
+
+print"\n Hear you have to enter number one by one,\n First turn is your turn and then computer turn";
+print"\n You both have two chances";
+print"\n 1st step : Your 1st chance: Please enter any digit number: ";
+$user_no = <>;
+$com_no = 99 - $user_no;
+print"\n 2nd step: computer 1st chance:                                 + $com_no";
+print"\n 3rd step : Your 2nd chance: Please enter any digit number: ";
+$user_no1 =<>;
+$com_no1 = 99 - $user_no1;
+print"\n 4th step: computer 2nd chance:                                 + $com_no1 ";
+print"\n\n Answer is 198";
+
+
+
